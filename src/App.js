fef3e9867d273717de1f371e0521ad9de2d26407
kenichi miyata <miyataken999@bpmboxes.com>
2022-10-03 15:57:35 +0000

    update

--- src/App.js
+++ src/App.js
@@ -1,8 +1,14 @@
 import './App.css';
+import { 
+  ActionCardCollection,NavBar, MarketingFooter 
+} from './ui-components';
 
 function App() {
   return (
     <div className="App">
+      <NavBar />
+      <ActionCardCollection />
+      <MarketingFooter />
     </div>
   );
 }
